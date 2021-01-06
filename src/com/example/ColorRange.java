244d6614f3545f2f47df645bb0260f0e54ddefed
Jamison2004 <Jamison.mcculloch-cop@ocsbstudent.ca>
2021-01-06 11:54:23 -0500

    Update ColorRange.java

--- src/com/example/ColorRange.java
+++ src/com/example/ColorRange.java
@@ -11,5 +11,36 @@ public class ColorRange {
 
     public static void main(String args[]) {
         
+      Scanner input = new Scanner(System.in);
+        System.out.println("Enter a color code.");
+        int num;
+        num = Integer.parseInt(input.next());
+     input.close();
+      
+     if (num<380){
+          System.out.println("The entered waveleth is not part of the visible spectrum");
+          
+      } else if (num<450){
+          System.out.println("The colour is Violet");
+          
+      } else if (num<495){
+          System.out.println("The colour is Blue");
+          
+      }else if (num<570){
+          System.out.println("The colour is Green");
+          
+      } else if (num<590){
+          System.out.println("The colour is Yellow");
+          
+      } else if (num<620){
+          System.out.println("The colour is Orange");
+          
+      } else if (num<750){
+          System.out.println("The colour is Red");
+          
+      } else{
+          System.out.println("The entered waveleth is not part of the visible spectrum"); 
+          
+      }
         }
     }
